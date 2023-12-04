f7438e85c86f7799b37cb8de48367c0b63e7fe7b
Federico Lochbaum <federico.lochbaum@gmail.com>
2023-12-04 02:55:17 -0300

    Day 4: Scratchcards

--- challenges/Day 4: Scratchcards/scratchcard.py
+++ challenges/Day 4: Scratchcards/scratchcard.py
@@ -1,10 +1,11 @@
 input_path = 'challenges/Day 4: Scratchcards/input'
 
-sum = 0
+_sum, cards, matches = 0, [], []
 
 with open(input_path) as f:
   for line in f:
     winning_d = {}
+    cards.append(1)
     card_i, nums = line[:-1].split(': ')
     winning, having = nums.split(' | ')
     for winning_num in winning.split(' '): winning_d[winning_num] = False
@@ -13,7 +14,11 @@ with open(input_path) as f:
     for n in having.split(' '):
       if n in winning_d and (not winning_d[n]): winning_d[n] = True; winners += 1
     
-    sum += pow(2, winners - 1) if winners > 0 else 0
-    
-  print('Part 1: ', sum)
+    _sum += pow(2, winners - 1) if winners > 0 else 0
+    matches.append(winners)
 
+for i in range(len(matches)):
+  for j in range(i + 1, min(len(matches), i + 1 + matches[i])): cards[j] += cards[i]
+
+print('Part 1: ', _sum)
+print('Part 2:', sum(cards))
