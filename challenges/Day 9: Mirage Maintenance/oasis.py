4b095db7c9c800ff412f4914a6afa201a198bfe0
Federico Lochbaum <federico.lochbaum@gmail.com>
2023-12-09 22:23:28 -0300

    Part 2

--- challenges/Day 9: Mirage Maintenance/oasis.py
+++ challenges/Day 9: Mirage Maintenance/oasis.py
@@ -1,5 +1,7 @@
 input_path = 'challenges/Day 9: Mirage Maintenance/input'
 
+part1 = 0; part2 = 0; memo1 = {}; memo2 = {}
+
 def next_below(history):
   below = []
   if len(history) == 1: return history[0]
@@ -7,21 +9,32 @@ def next_below(history):
     below.append(history[i] - history[i - 1])
   
   return below
-      
+
+def before_value(history, memo):
+  if all(map(lambda x: x == 0, history)): return 0
+  
+  if (not str(history) in memo):
+    below = next_below(history)
+    before_before = before_value(below, memo)
+    memo[str(history)] = history[0] - before_before
+
+  return memo[str(history)]
+
 def next_value(history, memo):
   if all(map(lambda x: x == 0, history)): return 0
 
   if (not str(history) in memo):
     below = next_below(history)
-    next_before = (next_value(below, memo))
+    next_before = next_value(below, memo)
     memo[str(history)] = history[-1] + next_before
 
   return memo[str(history)]
 
-_sum = 0; memo = {}
 with open(input_path) as f:
   for line in f:
     history = list(map(int, line[:-1].split(' ')))
-    _sum += next_value(history, memo)
+    part1 += next_value(history, memo1)
+    part2 += before_value(history, memo2)
 
-print('Part 1: ', _sum)
+print('Part 1: ', part1)
+print('Part 2: ', part2)
