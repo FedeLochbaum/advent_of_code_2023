29c29d3c0af0dd46c39879f631891e2b467185c7
Federico Lochbaum <federico.lochbaum@gmail.com>
2023-12-16 08:21:13 -0300

    Day 11, part 2

--- challenges/Day 11: Cosmic Expansion/observatory.py
+++ challenges/Day 11: Cosmic Expansion/observatory.py
@@ -1,15 +1,6 @@
 input_path = 'challenges/Day 11: Cosmic Expansion/input'
 from utils import grid_from_file
 
-def expand_grid(graph):
-  empty_rows = [i for i, row in enumerate(graph) if '#' not in row]
-  empty_cols = [j for j in range(len(graph[0])) if all(row[j] != '#' for row in graph)]
-  for i in reversed(empty_rows): graph.insert(i, graph[i].copy())
-  for j in reversed(empty_cols):
-    for row in graph: row.insert(j, row[j])
-
-  return graph
-
 def galaxies(grid):
   res = []; node = 0;
   for r in range(len(grid)):
@@ -18,29 +9,36 @@ def galaxies(grid):
       node +=1
   return res
 
-grid = expand_grid(grid_from_file(input_path))
+def expand(universe):
+  expanded_rows, expanded_cols = set(), set()
+  for i in range(len(universe)):
+    if all(char == '.' for char in universe[i]): expanded_rows.add(i)
+  num_cols = len(universe[0])
+  for j in range(num_cols):
+    if all(row[j] == '.' for row in universe): expanded_cols.add(j)
 
+  return expanded_rows, expanded_cols
+
+grid = grid_from_file(input_path)
+expanded_rows, expanded_cols = expand(grid)
 _galaxies = galaxies(grid)
 
-# def valid_point(point):
-#   if point[0] < 0 or point[0] >= len(grid): return False
-#   if point[1] < 0 or point[1] >= len(grid[0]): return False
-#   return True
-    
-# def compute_mappers(grid): 
-#   indexes = {}; _neighbors = {}; current = 0
-#   for r in range(len(grid)):
-#     for c in range(len(grid[r])):
-#       point = (r, c)
-#       indexes[str(point)] = current
-#       _neighbors[current] = list(filter(valid_point, neighbors(r, c)))
-#       current += 1
-#   return indexes, _neighbors
-
-# mapper_indexex, mapper_neighbors = compute_mappers(grid) # { point -> index }, { index -> [ point ] }
-# distances = floyd_warshall(mapper_indexex, mapper_neighbors)
-
 manhattan_distance = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def dist_with_expansion(p1, p2, expansion):
+  offset = 0
+  mini = min(p1[0], p2[0])
+  maxi = max(p1[0], p2[0])
+  minj = min(p1[1], p2[1])
+  maxj = max(p1[1], p2[1])
+
+  for _ in range(mini + 1, maxi):
+    if _ in expanded_rows: offset += ( expansion - 1 )
+  
+  for _ in range(minj + 1, maxj):
+    if _ in expanded_cols: offset += ( expansion - 1 )
+
+  return manhattan_distance(p1, p2) + offset
 
 _sum = 0
 seen = set()
@@ -48,7 +46,7 @@ for g1 in _galaxies:
   for g2 in _galaxies:
     if g1 == g2: continue
     if str((g1, g2)) in seen or str((g2, g1)) in seen: continue
-    _sum += manhattan_distance(g1, g2)
+    _sum += dist_with_expansion(g1, g2, 1000000)
     seen.add(str((g1, g2)))
 
-print('Part 1: ', _sum)
+print('Part 2: ', _sum)
