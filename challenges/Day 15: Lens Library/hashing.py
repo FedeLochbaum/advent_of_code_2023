5d87715c59afbd8dab30726f18cad5744cc811c2
Federico Lochbaum <federico.lochbaum@gmail.com>
2023-12-25 10:39:43 -0300

    Part 2

--- challenges/Day 15: Lens Library/hashing.py
+++ challenges/Day 15: Lens Library/hashing.py
@@ -3,13 +3,27 @@ from functools import reduce
 
 hash = lambda _str: reduce(lambda h, c: ((h + ord(c)) * 17) % 256, _str, 0)
 
-sum = 0
+part1, part2, hash_map = 0, 0, [{} for _ in range(256)]
+
+def remove_cmd(cmd):
+  label = cmd[:-1]
+  box = hash_map[hash(label)]
+  if label in box: box.pop(label)
+
+def add_cmd(cmd):
+  label, focal_length = cmd.split('=')
+  box = hash_map[hash(label)]
+  box[label] = int(focal_length)
 
 f = open(input_path)
 line = f.readline()[:-1]
 for cmd in line.split(','):
-  # var, num = cmd.split('=')
-  sum += hash(cmd)
-  # print('hash(HASH)', hash(cmd))
+  part1 += hash(cmd)
+  (remove_cmd if '-' in cmd else add_cmd)(cmd)
 
-print('Part 1: ', sum)
+for i, box in enumerate(hash_map):
+  for j, lens in enumerate(box.items()):
+    part2 += ((i+1) * (j+1) * lens[1])
+
+print('Part 1: ', part1)
+print('Part 2: ', part2)
